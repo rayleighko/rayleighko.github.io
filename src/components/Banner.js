6bdf8a2244f3feec218158b4d57adfb4f5752eff
Rayleigh <gmj1197@gmail.com>
2019-01-09 21:47:29 +0900

    fix: Modify Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,20 +1,26 @@
 import React from 'react'
 
-const Banner = (props) => (
-    <section id="banner" className="major">
-        <div className="inner">
-            <header className="major">
-                <h1>Hi, my name is Forty</h1>
-            </header>
-            <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
-                <ul className="actions">
-                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
-                </ul>
-            </div>
-        </div>
-    </section>
+const Banner = props => (
+  <section id="banner" className="major">
+    <div className="inner">
+      <header className="major">
+        <h1>Hi, my name is Rayleigh Ko, 고명진</h1>
+      </header>
+      <div className="content">
+        <p>
+          평생학습(Lifelong learning), 민이호학 불치하문(敏而好學 不恥下問)
+          <br />위 두 문장의 의미는 저의 삶에 그대로 녹아있습니다.
+        </p>
+        <ul className="actions">
+          <li>
+            <a href="#one" className="button next scrolly">
+              GET MY LOG
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
 )
 
 export default Banner
