8625787540ffed059d449c1c37703e2d7e1e9c9f
Rayleigh <gmj1197@gmail.com>
2019-02-11 19:18:23 +0900

    env: Add dot-env contents for github

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,11 @@
+// Initialize dotenv
+require('dotenv').config({
+  path: '.env', // or '.env'
+})
+
+// And then you can use the config in gatsby-config.js
+const config = require('gatsby-plugin-config')
+
 module.exports = {
   siteMetadata: {
     title: 'RayLog',
@@ -16,6 +24,22 @@ module.exports = {
         theme_color: '#663399',
         display: 'minimal-ui',
         icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'queries',
+        path: `${__dirname}/src/queries/`,
+      },
+    },
+    {
+      resolve: '@wyze/gatsby-source-graphql',
+      options: {
+        headers: {
+          authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        },
+        url: 'https://api.github.com/graphql',
       },
     },
     'gatsby-plugin-sass',
